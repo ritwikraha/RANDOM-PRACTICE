3eca1d364902cdc55eab032fbc5d0b3ae34231ad
Ritwik Raha <rtzdzn@gmail.com>
2019-04-05 10:23:15 +0530

    OPENCV codes added

---
+++ DL_Python/CNN/ip_ocv.py
@@ -0,0 +1,31 @@
+import cv2
+
+image = cv2.imread('stop_sign.jpg')
+
+b = image.copy()
+# set green and red channels to 0
+b[:, :, 1] = 0
+b[:, :, 2] = 0
+
+
+g = image.copy()
+# set blue and red channels to 0
+g[:, :, 0] = 0
+g[:, :, 2] = 0
+
+r = image.copy()
+# set blue and green channels to 0
+r[:, :, 0] = 0
+r[:, :, 1] = 0
+
+
+# RGB - Blue
+cv2.imshow('B-RGB', b)
+
+# RGB - Green
+cv2.imshow('G-RGB', g)
+
+# RGB - Red
+cv2.imshow('R-RGB', r)
+
+cv2.waitKey(0)
