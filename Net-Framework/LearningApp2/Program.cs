ba563013a28837782950b91de7b79d5adc047be1
ritwikraha <rtzdzn@gmail.com>
2021-03-11 11:35:51 +0530

    some more c# learnings

---
+++ Net-Framework/LearningApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string phrase = "Hey vs";
+
+		Console.WriteLine("Length of the sentence is " + phrase.Length);
+		Console.WriteLine(phrase.ToUpper());
+		Console.WriteLine(phrase.Substring(0, 5));
+	}
+}
---
+++ Net-Framework/LearningApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string phrase = "name";
+
+		Console.WriteLine("Enter your " + phrase);
+		string name = Console.ReadLine();
+		Console.WriteLine("Hello idiot" + name);
+	}
+}
