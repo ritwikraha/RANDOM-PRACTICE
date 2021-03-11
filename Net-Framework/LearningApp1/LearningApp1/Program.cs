5770d7d0c035aa138018092936cdc5d0a3ed00c2
ritwikraha <rtzdzn@gmail.com>
2021-03-11 11:14:30 +0530

    day 2 of c#

--- Net-Framework/LearningApp1/LearningApp1/Program.cs
+++ Net-Framework/LearningApp1/LearningApp1/Program.cs
@@ -10,7 +10,15 @@ namespace LearningApp1
         {
             // printing hello world
             // use ReadLine() to keep the window open
-            Console.WriteLine("Hello World!");
+            string words = "I am Ritwik, I guess.";
+            char grade = 'a';
+            int year = 97;
+            double cgpa = 8.97;
+
+            Console.WriteLine("Hello"+words);
+            Console.WriteLine("My grade is " + grade);
+            Console.WriteLine("My year is " + year);
+            Console.WriteLine("And the cgpa is" + cgpa);
             Console.ReadLine();
         }
     }
